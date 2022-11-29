06f4e47c33e308d00fda6205018f8252f2e0cea6
filol <filolog999333@mail.ru>
2022-11-29 22:32:23 +1000

    урок

--- src/test/java/WindowTest.java
+++ src/test/java/WindowTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class WindowTest {
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WindowType;
+
+public class WindowTest extends AbstractTest {
+    @Test
+    void test() throws InterruptedException{
+        System.out.println(getDriver().getWindowHandle());
+        String originalWindow = getDriver().getWindowHandle();
+
+        getDriver().switchTo().newWindow(WindowType.WINDOW);// Открываем новое окно
+        getDriver().navigate().to("https://www.google.com");// и в этом окне открываем страницу
+
+        Thread.sleep(10000);
+        getDriver().switchTo().window(originalWindow);
+
+        for (String windowHandle: getDriver().getWindowHandles()){
+            System.out.println(windowHandle);
+        }
+        getDriver().close();
+    }
 }
